15cfebc930d0fb3ca25d1983511f43df0b2c000d
nucleus0128 <95115216+nucleus0128@users.noreply.github.com>
2021-12-15 00:35:41 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-$(function () {
-    // カレンダー
-    $(function () {
-        $('input[name="date"]').datepicker({
-            dateFormat: 'yy/mm/dd',
-        });
-    });
-
     // 参加人数分の氏名欄を生成
     $('#form-number').click(function () {
         $('#form-name').empty();
