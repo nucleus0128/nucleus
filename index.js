a7b333cbed97288a628977145d3ee94dd086d612
nucleus0128 <95115216+nucleus0128@users.noreply.github.com>
2021-12-15 00:15:57 +0900

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ $(function () {
         })
         names = names.slice(0, -1);
 
-        var msg = `希望日：${date}\希望時間：${time}\n人数：${number}\n氏名：${names}\電話番号：${tel}`;
+        var msg = `希望日：${date}<br>\希望時間：${time}<br>\n人数：${number}<br>\n氏名：${names}<br>\電話番号：${tel}`;
         sendText(msg);
 
         return false;
