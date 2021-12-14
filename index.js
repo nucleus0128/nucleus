a7e0038886aa13d7a7c0635b7b8f20ff4340c07c
nucleus0128 <95115216+nucleus0128@users.noreply.github.com>
2021-12-14 22:24:31 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 $(function () {
     // カレンダー
     $(function () {
-        $('input[name="date01"]')$('#datepicker').datepicker({
+        $('input[name="date01"]')$
+        
+('#datepicker').datepicker({
             
                 monthNames: ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"]
             
