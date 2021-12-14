dc1b1a09a4cd272fccd8b722c7b54918e0a1611a
nucleus0128 <95115216+nucleus0128@users.noreply.github.com>
2021-12-15 00:41:29 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+   $(function () {
+    // カレンダー
+    $(function () {
+        $('input[name="date"]').datepicker({
+            dateFormat: 'yy/mm/dd',
+        });
+    });
+
     // 参加人数分の氏名欄を生成
     $('#form-number').click(function () {
         $('#form-name').empty();
@@ -12,16 +20,14 @@
     // 送信
     $('form').submit(function () {
         var date = $('input[name="date"]').val();
-        var time = $('input[name="time"]').val();
         var number = $('input[name="number"]:checked').val();
         var names = '';
-        var tel = $('input[name="tel"]').val();
         $('#form-name').children().each(function (i, elm) {
             names += $(elm).val() + '、';
         })
         names = names.slice(0, -1);
 
-        var msg = `希望日：${date}\n希望時間：${time}\n人数：${number}\n氏名：${names}\n電話番号：${tel}`;
+        var msg = `希望日：${date}\n人数：${number}\n氏名：${names}`;
         sendText(msg);
 
         return false;
