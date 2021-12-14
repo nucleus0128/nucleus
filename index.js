28541a6497f574a21bc1928ea0958d87272ab36e
nucleus0128 <95115216+nucleus0128@users.noreply.github.com>
2021-12-14 23:53:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ $(function () {
     // 送信
     $('form').submit(function () {
         var date = $('input[name="date"]').val();
+        var time = $('input[name="time"]').val();
         var number = $('input[name="number"]:checked').val();
         var names = '';
         $('#form-name').children().each(function (i, elm) {
