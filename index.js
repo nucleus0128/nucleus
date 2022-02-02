6548dbc5346b81d823b2206f9c582a5a668af615
nucleus0128 <95115216+nucleus0128@users.noreply.github.com>
2022-02-03 00:47:54 +0900

    Update index.js

--- index.js
+++ index.js
@@ -29,13 +29,13 @@
         var names = '';
         var address = $('input[name="address"]').val();
         var tel = $('input[name="tel"]').val();
-       　　var text = $('input[name="text"]').val();
+       　　var address01 = $('input[name="address01"]').val();
         $('#form-name').children().each(function (i, elm) {
             names += $(elm).val() + '、';
         })
         names = names.slice(0, -1);
 
-        var msg = `・第一希望日：${date01}\n希望時間：${time01}\n・第二希望日：${date02}\n希望時間：${time02}\n・第三希望日：${date03}\n希望時間：${time03}\n人数：${number}\n氏名：${names}\n住所：${address}\n電話番号：${tel}`\n備考（ご要望やご希望メニューなど）：${text};
+        var msg = `・第一希望日：${date01}\n希望時間：${time01}\n・第二希望日：${date02}\n希望時間：${time02}\n・第三希望日：${date03}\n希望時間：${time03}\n人数：${number}\n氏名：${names}\n住所：${address}\n電話番号：${tel}`\n備考（ご要望やご希望メニューなど）：${address};
         sendText(msg);
 
         return false;
