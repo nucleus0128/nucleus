410a1a9118cf74d95e49875be2e9f80b56bcb924
nucleus0128 <95115216+nucleus0128@users.noreply.github.com>
2022-02-03 00:36:55 +0900

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@
         })
         names = names.slice(0, -1);
 
-        var msg = `・第一希望日：${date01}\n希望時間：${time01}\n・第二希望日：${date02}\n希望時間：${time02}\n・第三希望日：${date03}\n希望時間：${time03}\n人数：${number}\n氏名：${names}\n住所：${address}\n電話番号：${tel}`\n備考（ご要望、ご希望メニューなど）：${text};
+        var msg = `・第一希望日：${date01}\n希望時間：${time01}\n・第二希望日：${date02}\n希望時間：${time02}\n・第三希望日：${date03}\n希望時間：${time03}\n人数：${number}\n氏名：${names}\n住所：${address}\n電話番号：${tel}`\n備考（ご要望やご希望メニューなど）：${text};
         sendText(msg);
 
         return false;
